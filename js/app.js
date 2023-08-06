2c5047af31ca5c949b0f9c33b34ae10907ada458
hemachy <122871924+hemachy@users.noreply.github.com>
2023-08-06 14:08:43 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+
+
+// Leather Jackets Quantity
+
+$(function(){
+function increaseValue() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+  }
+  
+  function decreaseValue() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('number').value = value;
+  }
+})
+
+
